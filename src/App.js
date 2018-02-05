51a1f09f5133acea4fc216022e2d1dcbfefaf8e3
John Murphy <murf2073@gmail.com>
2018-02-05 17:35:17 -0500

    L38 button/method lesson

--- src/App.js
+++ src/App.js
@@ -3,12 +3,27 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'John', age: 27},
+      {name: 'Mike', age: 22},
+      {name: 'Pat', age: 25}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked!');
+  }
+
   render() {
    return (
      <div className="App">
        <h1>Hi, I'm a React App</h1>
-       <p>This is really working</p> 
-       <Person />
+       <p>This is really working</p>
+       <button onClick={this.switchNameHandler}>Switch Name</button> 
+       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Sleep</Person>
+       <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
      </div>
    );
    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App?'));
