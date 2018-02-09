c88bd520065141295e4884687a7299b622e3bdb3
John Murphy <murf2073@gmail.com>
2018-02-09 12:04:37 -0500

    L53 delete persons

--- src/App.js
+++ src/App.js
@@ -13,15 +13,10 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = () => {
-    //console.log('Was clicked!');
-    this.setState({    
-      persons: [
-        {name: 'John', age: 21},
-        {name: 'Mike', age: 21},
-        {name: 'Pat', age: 21}
-      ] 
-    })
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
   }
 
   nameChangedHandler = (event) => {
@@ -54,8 +49,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age} />
+          {this.state.persons.map((person, index) => {
+            return <Person 
+              click={() => this.deletePersonHandler(index)}
+              name={person.name} 
+              age={person.age} />
           })} 
         </div>    
       );
